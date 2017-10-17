88f0280040d9b4b773788caa8844be3bb10a6cc3
trevphil <cheezemaker37@gmail.com>
2017-10-17 18:36:01 -0400

    Attempt to make bot work on Slack channel

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const builder = require('botbuilder');
 
-// Create chat bot
-const connector = new builder.ConsoleConnector(
-/*
-    {
-        appId:
-        appPassword:
-    }
-*/
-).listen();
-const bot = new builder.UniversalBot(connector, function (session) {
-  // Bot dialog
-  session.send('You said: %s', session.message.text);
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: null,
+    appPassword: null
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
 });
