e6f95539ee295c5b6d0b9df63f9a8928654b880a
mrubes375 <mrubes375@gmail.com>
2017-10-18 12:30:57 -0400

    Fix eslint errors

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-require('dotenv').config()
+require('dotenv').config();
 
-var restify = require('restify')
-var builder = require('botbuilder')
+const restify = require('restify');
+const builder = require('botbuilder');
 
 // Setup Restify Server
-var server = restify.createServer()
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-  console.log('%s listening to %s', server.name, server.url)
-})
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log('%s listening to %s', server.name, server.url);
+});
 
 // Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
+const connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
-  appPassword: process.env.MICROSOFT_APP_PASSWORD
-})
+  appPassword: process.env.MICROSOFT_APP_PASSWORD,
+});
 
 // Listen for messages from users
-server.post('/api/messages', connector.listen())
+server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-  session.send('You said: %s', session.message.text)
-})
+/* Receive messages from the user and respond by echoing
+each message back (prefixed with 'You said:') */
+
+const bot = new builder.UniversalBot(connector, ((session) => {
+  session.send('You said: %s', session.message.text);
+}));
