e9eee3034f3a0e5b201a721a039ea1f12c4e1e48
Matthew Rubenstein <matthew.rubenstein@weightwatchers.com>
2018-01-24 14:11:58 -0500

    Create global vars and update deps

--- logicbroker-service.js
+++ logicbroker-service.js
@@ -3,7 +3,7 @@ module.exports = {
         new Promise(function (resolve) {
             const response = '{ "data" : "this is a fake response" }';
             // complete promise with a timer to simulate async response
-            setTimeout(function () { resolve(response); }, 2000);
+            setTimeout(function () { resolve(response); }, 1000);
         })
     }
 };
