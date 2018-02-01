5294e7919e80d2cb0e8d6d42eeb558b1a6abd6d9
qvissak <quinn.vissak@uconn.edu>
2018-02-01 11:10:28 -0500

    Add Additional API Skeleton

--- src/apis/logicBroker.js
+++ src/apis/logicBroker.js
@@ -1,12 +1,17 @@
+const acknowledgement = require('./acknowledgement');
 const inventory = require('./inventory');
 const invoice = require('./invoice');
 const order = require('./order');
+const status = require('./status');
 const shipment = require('./shipment');
 const product = require('./product');
+
 module.exports = {
+  acknowledgement,
   inventory,
   invoice,
   order,
   product,
+  status,
   shipment
 }
