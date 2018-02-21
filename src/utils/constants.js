d2764cc0a7564c80eccb585902186ce48374f6c9
Trevor Phillips <trevor.j.phillips@uconn.edu>
2018-02-21 13:54:07 -0500

    String to int and vice versa for status codes (#15)
    * String to int and vice versa for status codes
    
    * String to int and vice versa for status codes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,15 +20,29 @@ module.exports = {
       },
     },
   },
-  orderStatuses: {
+  statusStr2Int: {
+    /* Omitted draft (code -1) */
     New: 0,
     Submitted: 100,
     'Ready to Acknowledge': 150,
+    Processing: 200,
     'Ready to Ship': 500,
     'Ready to Invoice': 600,
     Complete: 1000,
     Cancelled: 1100,
     Failed: 1200,
     Ignored: 1400,
+  },
+  statusInt2Str: {
+    /* Omitted draft (code -1) */
+    0: 'New',
+    100: 'Submitted',
+    150: 'Ready to Acknowledge',
+    200: 'Processing',
+    600: 'Ready to Ship',
+    1000: 'Complete',
+    1100: 'Cancelled',
+    1200: 'Failed',
+    1400: 'Ignored',
   },
 };
