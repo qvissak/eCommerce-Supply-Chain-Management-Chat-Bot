e696ad4daa6b37c9748a041372de81c9c427b0a0
Siena Biales <siena.biales@uconn.edu>
2018-03-07 14:29:21 -0500

    Add update order status dialog
    Change constants to reflect new dialog and rename statuses. Update LUIS to also reflect these changes.

--- src/dialogs/dialogStore.js
+++ src/dialogs/dialogStore.js
@@ -3,10 +3,12 @@ const order = require('./orders');
 const login = require('./login');
 const help = require('./help');
 const hello = require('./hello');
+const updateOrderStatus = require('./updateOrderStatus');
 
 module.exports = {
   order,
   login,
   help,
   hello,
+  updateOrderStatus,
 };
