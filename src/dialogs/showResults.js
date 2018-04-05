43405500ee2dee325d09f346d49d4c2faa42b21d
trevphil <cheezemaker37@gmail.com>
2018-04-05 11:22:31 -0400

    Fixed linting issues

--- src/dialogs/showResults.js
+++ src/dialogs/showResults.js
@@ -1,9 +1,7 @@
 const builder = require('botbuilder');
-const apiStore = require('../apis/apiStore');
 const createCards = require('./helpers/cards');
-const { logger } = require('../utils/logger');
 const orderAPIHelper = require('./helpers/orders');
-const { entities, statusStr2Int, statusInt2Str, dialogs } = require('../utils/constants');
+const { statusInt2Str, dialogs } = require('../utils/constants');
 
 const displayOrderResponse = (session, resp, statusStr) => {
   const status = statusStr.toDialogString().toLowerCase();
@@ -18,19 +16,21 @@ const displayOrderResponse = (session, resp, statusStr) => {
 };
 
 module.exports = [
-     (session, arg) => {
-     	  session.dialogData.payload = arg.payload;
-     	  session.dialogData.statusStr = arg.statusStr;
-     	  displayOrderResponse(session, session.dialogData.payload.Records, session.dialogData.statusStr);
-          builder.Prompts.choice(session, `Would you like to see more?`, `Yes|No`, { listStyle: 3 });
-      },
-      (session, results) => {
-          if(results.response.entity === `Yes`){
-          	session.send(`Ok! I'll show you more.`);
-            session.replaceDialog(dialogs.showResults.id, {payload: session.dialogData.payload, statusStr: session.dialogData.statusStr});
-          }
-          else{
-            session.endDialog(`Ok, I won't show anymore.`)
-          }
-      },
+  (session, arg) => {
+    session.dialogData.payload = arg.payload;
+    session.dialogData.statusStr = arg.statusStr;
+    displayOrderResponse(session, session.dialogData.payload.Records, session.dialogData.statusStr);
+    builder.Prompts.choice(session, 'Would you like to see more?', 'Yes|No', { listStyle: 3 });
+  },
+  (session, results) => {
+    if (results.response.entity === 'Yes') {
+      session.send("Ok! I'll show you more.");
+      session.replaceDialog(
+        dialogs.showResults.id,
+        { payload: session.dialogData.payload, statusStr: session.dialogData.statusStr }
+      );
+    } else {
+      session.endDialog("Ok, I won't show anymore.");
+    }
+  },
 ];
