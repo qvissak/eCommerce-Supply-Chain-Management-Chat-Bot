6b5b2d590b8021d187a53ed734096df49bd40976
Siena Biales <siena.biales@uconn.edu>
2018-04-25 13:41:22 -0400

    Add randomized help query dialog

--- src/dialogs/help.js
+++ src/dialogs/help.js
@@ -1,6 +1,9 @@
 // help dialog
+const smartResponse = require('./smartResponse');
+
 module.exports = (session) => {
-  session.send('Some commands you can ask me...');
+  const helpDialog = smartResponse.helpResponse();
+  session.send(helpDialog);
   session.send('Show me open orders from two months ago\n\n' +
     'Show me order XYZ123\n\nBilling address of order ABC321\n\n' +
     'Update the status of order XYZ123 to Completed');
